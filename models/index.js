059064693db306435952310b319c5c8ca2949c63
mohamedderbali <mohamed.derbali@esprit.tn>
2022-11-27 23:40:03 +0100

    manytomany

--- models/index.js
+++ models/index.js
@@ -23,9 +23,23 @@ db.sequelize = sequelize;
 
 db.user = require("./user.model.js")(sequelize, Sequelize);
 db.classroom = require("./classroom.model.js")(sequelize, Sequelize);
+db.tutorial = require("./tutorial.model.js")(sequelize, Sequelize);
+db.tag = require("./tag.model.js")(sequelize, Sequelize);
+
+db.tag.belongsToMany(db.tutorial, {
+  through: "tutorial_tag",
+  as: "tutorials",
+  foreignKey: "tag_id",
+});
+db.tutorial.belongsToMany(db.tag, {
+  through: "tutorial_tag",
+  as: "tags",
+  foreignKey: "tutorial_id",
+});
 db.classroom.hasMany(db.user, { as: "users" });
 db.user.belongsTo(db.classroom, {
   foreignKey: "classroomId",
   as: "classroom",
 });
+
 module.exports = db;
